5f251e7ed39dda7c1e5330562ede3eea3e81f1e3
Himel-010 <kft776@gmail.com>
2024-09-25 23:09:48 +0600

    Final Update

---
+++ js/utilities.js
@@ -0,0 +1,32 @@
+function donate(currentDonation, receivedDonation){
+    let totalAmount = Number(document.getElementById('totalAmount').innerText);
+    if(totalAmount < receivedDonation || isNaN(receivedDonation) || receivedDonation <= 0){
+        alert('Invalid Donation Amount');
+        return;
+    }
+    const remainAmount = totalAmount - receivedDonation;
+    document.getElementById('totalAmount').innerText = remainAmount;
+
+    return currentDonation + receivedDonation;
+
+}
+
+document.getElementById('page').addEventListener('click', function(event) {
+    const buttons = document.getElementsByClassName('button');
+    
+    if (event.target.tagName === 'BUTTON') {
+        for (let item of buttons) {
+            item.style.backgroundColor = '#F9F7F3';
+        }
+        event.target.style.backgroundColor = '#B4F461';
+    }
+});
+
+function showPage(id){
+    let allPages = document.getElementsByTagName('main');
+
+    for (let page of allPages){
+        page.classList.add('hidden');
+    }
+    document.getElementById(id).classList.remove('hidden');
+}
